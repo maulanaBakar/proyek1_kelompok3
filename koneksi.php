9afa819ae5ca9a727e1121229612b7989a1e1aca
ekafadilla2006-source <ekafadilla2006@gmail.com>
2026-04-08 20:49:47 +0700

    perbaikan

--- koneksi.php
+++ koneksi.php
@@ -5,7 +5,7 @@ $pass = "";
 $db   = "proyek_2paksi"; 
 $user = "AMANDA";
 $pass = "12345";
-$db   = "proyek_2paksi"
+$db   = "proyek_2paksi";
 
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
