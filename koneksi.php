40a2aba677fb5d758c06ebf8e3072be66a8d719c
ekafadilla2006-source <ekafadilla2006@gmail.com>
2026-04-08 20:59:52 +0700

    git commit -m "Update koneksi.php with correct database credentials"``

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,8 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "proyek_2paksi"; 
-$user = "AMANDA";
-$pass = "12345";
+$user = "root";        
+$pass = "";             
 $db   = "proyek_2paksi";
-
-
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if (!$koneksi) {
