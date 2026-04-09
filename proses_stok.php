9e55e504f63a2954a94bd33192f50387c6b79f33
ekafadilla2006-source <ekafadilla2006@gmail.com>
2026-04-09 11:48:41 +0700

    stok update

---
+++ proses_stok.php
@@ -0,0 +1,33 @@
+<?php
+include 'koneksi.php';
+
+if (isset($_POST['save'])) {
+    $id_produk    = $_POST['id_produk']; 
+    $nama_produk  = $_POST['nama_produk'];
+    $stok         = $_POST['stok'];
+    $kategori     = $_POST['kategori'];
+    $harga_jual   = $_POST['harga_jual'];
+
+    if (empty($id_produk)) {
+        
+        $query = "INSERT INTO produk (nama_produk, harga_satuan, stok, kategori) 
+                  VALUES ('$nama_produk', '$harga_jual', '$stok', '$kategori')";
+    } else {
+     
+        $query = "UPDATE produk SET 
+                  nama_produk = '$nama_produk', 
+                  harga_satuan = '$harga_jual', 
+                  stok = '$stok', 
+                  kategori = '$kategori' 
+                  WHERE id_produk = '$id_produk'";
+    }
+
+    $exec = mysqli_query($koneksi, $query);
+
+    if ($exec) {
+        echo "<script>alert('Data Berhasil Disimpan'); window.location='stok.php';</script>";
+    } else {
+        echo "Gagal menyimpan data: " . mysqli_error($koneksi);
+    }
+}
+?>
