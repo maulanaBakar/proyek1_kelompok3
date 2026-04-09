a1d12bbaaa6588e4e79960dd615ccd247374d22f
ZulfahAmanda2907 <2503044@student.polindra.ac.id>
2026-04-09 12:26:25 +0700

    update

--- kasir.php
+++ kasir.php
@@ -1,7 +1,95 @@
-<?php 
+<?php
+include 'koneksi.php';
 session_start();
+
+// 1. Cek Login
 if($_SESSION['status'] != "login"){
-    header("location:login.php");
+    header("location:login.php?pesan=belum_login");
+    exit();
+}
+
+// 2. Inisialisasi Keranjang
+if(!isset($_SESSION['keranjang'])) {
+    $_SESSION['keranjang'] = [];
+}
+
+// --- 3. LOGIKA SISTEM (TAMBAH, KURANG, HAPUS, BAYAR) ---
+
+// A. Tambah atau Tambah Kuantitas (+)
+if(isset($_GET['aksi']) && $_GET['aksi'] == "tambah") {
+    $id = $_GET['id_produk'];
+    $data = mysqli_query($koneksi, "SELECT * FROM produk WHERE id_produk='$id'");
+    $p = mysqli_fetch_assoc($data);
+
+    if($p) {
+        if(isset($_SESSION['keranjang'][$id])) {
+            $_SESSION['keranjang'][$id]['qty'] += 1;
+        } else {
+            $_SESSION['keranjang'][$id] = [
+                'nama'  => $p['nama_produk'],
+                'harga' => $p['harga_satuan'],
+                'qty'   => 1
+            ];
+        }
+    }
+    header("location:kasir.php");
+    exit();
+}
+
+// B. Kurangi Kuantitas (-)
+if(isset($_GET['aksi']) && $_GET['aksi'] == "kurang") {
+    $id = $_GET['id_produk'];
+    if(isset($_SESSION['keranjang'][$id])) {
+        $_SESSION['keranjang'][$id]['qty'] -= 1;
+        if($_SESSION['keranjang'][$id]['qty'] <= 0) {
+            unset($_SESSION['keranjang'][$id]);
+        }
+    }
+    header("location:kasir.php");
+    exit();
+}
+
+// C. Hapus Item dari Keranjang
+if(isset($_GET['aksi']) && $_GET['aksi'] == "hapus") {
+    $id = $_GET['id_produk'];
+    unset($_SESSION['keranjang'][$id]);
+    header("location:kasir.php");
+    exit();
+}
+
+// D. Proses Selesaikan Pembayaran
+if(isset($_POST['proses_bayar'])) {
+    if(!empty($_SESSION['keranjang'])) {
+        $total_bayar = $_POST['total_bayar'];
+        $id_admin = $_SESSION['id_admin'] ?? 1;
+        $tgl = date('Y-m-d H:i:s');
+
+        // Simpan ke tabel transaksi
+        $query_t = "INSERT INTO transaksi (id_admin, tanggal_transaksi, total_pendapatan) 
+                    VALUES ('$id_admin', '$tgl', '$total_bayar')";
+        mysqli_query($koneksi, $query_t);
+        $id_transaksi = mysqli_insert_id($koneksi);
+
+        // Simpan ke detail_transaksi & Potong Stok
+        foreach($_SESSION['keranjang'] as $id_p => $item) {
+            $qty = $item['qty'];
+            $subtotal = $item['harga'] * $qty;
+            
+            // Menggunakan nama kolom sesuai database: jumlah_produk & subtotal
+            $query_d = "INSERT INTO detail_transaksi (id_transaksi, id_produk, jumlah_produk, subtotal) 
+                        VALUES ('$id_transaksi', '$id_p', '$qty', '$subtotal')";
+            mysqli_query($koneksi, $query_d);
+
+            // Update stok di tabel produk
+            mysqli_query($koneksi, "UPDATE produk SET stok = stok - $qty WHERE id_produk = '$id_p'");
+        }
+
+        unset($_SESSION['keranjang']);
+        echo "<script>
+            alert('PEMBAYARAN BERHASIL!\\nTotal: Rp " . number_format($total_bayar, 0, ',', '.') . "');
+            window.location='kasir.php';
+        </script>";
+    }
 }
 ?>
 
