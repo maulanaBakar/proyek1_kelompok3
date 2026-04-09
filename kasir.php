4ae72d7df917341693d22e1d298d81a242a56518
ekafadilla2006-source <ekafadilla2006@gmail.com>
2026-04-09 19:57:57 +0700

    k

--- kasir.php
+++ kasir.php
@@ -98,7 +98,7 @@ if(isset($_POST['proses_bayar'])) {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="style_kasir.css">
+    <link rel="stylesheet" href="kasir.css">
 </head>
 <body>
 
