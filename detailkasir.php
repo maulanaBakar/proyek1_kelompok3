e8cf9bb1e363f7681c13dd9854205cd45c4a92a7
ZulfahAmanda2907 <2503044@student.polindra.ac.id>
2026-04-09 19:50:26 +0700

    tes

--- detailkasir.php
+++ detailkasir.php
@@ -2,8 +2,7 @@
 include 'koneksi.php';
 $id = $_GET['id'];
 
-// Saya sesuaikan querynya. Jika error lagi, pastikan nama kolom 
-// di tabel detail_transaksi sudah benar (misal: jumlah_beli, qty, dll)
+
 $query = mysqli_query($koneksi, "SELECT p.nama_produk, d.jumlah_produk, d.subtotal 
                                 FROM detail_transaksi d 
                                 JOIN produk p ON d.id_produk = p.id_produk 
@@ -11,7 +10,7 @@ $query = mysqli_query($koneksi, "SELECT p.nama_produk, d.jumlah_produk, d.subtot
 
 if(mysqli_num_rows($query) > 0) {
     while($row = mysqli_fetch_assoc($query)) {
-        // Menggunakan 'jumlah_produk' sesuai struktur standar proyekmu
+        
         echo "<div class='item-row'>
                 <span>{$row['nama_produk']} (x{$row['jumlah_produk']})</span>
                 <span style='font-weight:700;'>Rp ".number_format($row['subtotal'], 0, ',', '.')."</span>
